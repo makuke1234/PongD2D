56dea76d4a4f6217dbd5c548a8bc2e258b2cc6d3
makuke1234 <makuke1234@gmail.com>
2021-12-31 12:04:00 +0200

    Documentation on logic

--- src/logic.h
+++ src/logic.h
@@ -47,20 +47,79 @@ typedef struct PongLogic
 
 } PongLogic_t;
 
+/**
+ * @brief Thread method that deals with all active game logic
+ * 
+ * @param param Pointer to PongLogic_t object
+ * @return DWORD Always 0
+ */
 DWORD WINAPI PongLogic_thread(LPVOID param);
 
-bool PongLogic_create(PongLogic_t * restrict logic, struct PongWnd * hwnd);
+/**
+ * @brief Creates PongLogic_t object
+ * 
+ * @param logic Pointer to PongLogic_t structure
+ * @param pong Pointer to PongWnd structure
+ * @return true On success
+ * @return false On failure
+ */
+bool PongLogic_create(PongLogic_t * restrict logic, struct PongWnd * pong);
+/**
+ * @brief Frees resources owned by PongLogic_t object
+ * 
+ * @param logic Pointer to PongLogic_t object
+ */
 void PongLogic_free(PongLogic_t * restrict logic);
 
+/**
+ * @brief Creates D2D assets
+ * 
+ * @param logic Pointer to PongLogic_t object
+ * @return true On success
+ * @return false On failure
+ */
 bool PongLogic_createAssets(PongLogic_t * restrict logic);
+/**
+ * @brief Destryos D2D assets
+ * 
+ * @param logic Pointer to PongLogic_t object
+ */
 void PongLogic_freeAssets(PongLogic_t * restrict logic);
 
+/**
+ * @brief Calculates absolute size rectangle of left pad
+ * 
+ * @param logic Pointer to PongLogic_t object
+ */
 void PongLogic_calcAbsLeftPad(PongLogic_t * restrict logic);
+/**
+ * @brief Calculates absolute size rectangle of right pad
+ * 
+ * @param logic Pointer to PongLogic_t object
+ */
 void PongLogic_calcAbsRightPad(PongLogic_t * restrict logic);
+/**
+ * @brief Calculates absolute location of ball ellipse
+ * 
+ * @param logic Pointer to PongLogic_t object
+ */
 void PongLogic_calcAbsBall(PongLogic_t * restrict logic);
 
+/**
+ * @brief Resets game logic to default state
+ * 
+ * @param logic Pointer to PongLogic_t object
+ */
 void PongLogic_reset(PongLogic_t * restrict logic);
 
+/**
+ * @brief "Clamps" a floating-point value to be within min and max
+ * 
+ * @param value Floating-point value to be clamped
+ * @param min Minimum value
+ * @param max Maximum value
+ * @return float Clamped value
+ */
 float clamp(float value, float min, float max);
 
 #endif
