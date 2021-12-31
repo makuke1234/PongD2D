b8344092200356291567e7cd26e7cb8dd1f916e4
makuke1234 <makuke1234@gmail.com>
2021-12-31 12:07:15 +0200

    Documentation on errors

--- src/pongerr.h
+++ src/pongerr.h
@@ -24,10 +24,33 @@ extern PongErr_e g_pongLastError;
 
 extern const wchar_t * c_errorIds[PongErr_num_errs];
 
+/**
+ * @brief Normalises error id to be within error id range
+ * 
+ * @param errId Error id to be normalised
+ * @return PongErr_e Normalised error id
+ */
 PongErr_e PongErr_norm(PongErr_e errId);
+/**
+ * @brief Returns error string from normalised error id
+ * 
+ * @param errId Error id to be normalised
+ * @return const wchar_t* Normalised error string
+ */
 const wchar_t * PongErr_msg(PongErr_e errId);
 
+/**
+ * @brief Brings up message box with message from normalised error id
+ * 
+ * @param pong Pointer to PongWnd_t object
+ * @param errId Error id to be normalised
+ */
 void pongErr(const PongWnd_t * restrict pong, PongErr_e errId);
+/**
+ * @brief Brings up message box with last occurred error message
+ * 
+ * @param pong Pointer to PongWnd_t object
+ */
 void pongLastErr(const PongWnd_t * restrict pong);
 
 
